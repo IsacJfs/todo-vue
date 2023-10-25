65a05b667ff267e70db1fb5d889ee65e1197168e
IsacJfs <isac.jfs@gmail.com>
2023-10-25 11:05:34 -0300

    :sparkles: [FEAT] adicionando texto quando não existem tarefas a serem exibidas

--- todo-vue/src/components/ListaDeTarefas.vue
+++ todo-vue/src/components/ListaDeTarefas.vue
@@ -2,7 +2,7 @@
 const props = defineProps(['tarefas'])
 </script>
 <template>
-    <ul class="list-group mt-4">
+    <ul class="list-group mt-4" v-if="props.tarefas.length > 0">
         <li class="list-group-item" v-for="tarefa in props.tarefas">
             <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
                 :id="tarefa.titulo" type="checkbox">
@@ -11,6 +11,9 @@ const props = defineProps(['tarefas'])
             </label>
         </li>
     </ul>
+    <div class="row mt-5" v-else>
+        <p class="text-center fs-3" >Não existem tarefas a serem exibidas</p>
+    </div>
 </template>
 
 <style scoped>
